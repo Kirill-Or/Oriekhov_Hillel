15a18ab218dbf2701ce7b8554cc06e0ba6c9e28b
Kirill <kr.oriekhov@gmail.com>
2021-05-01 00:36:59 +0300

    First commit

--- src/Lesson3/CheckOddEven.java
+++ src/Lesson3/CheckOddEven.java
@@ -1,4 +1,19 @@
 package Lesson3;
 
+import java.util.SortedMap;
+
 public class CheckOddEven {
+    public static void main(String[] args) {
+
+        int number = 7;
+        if ( number % 2 == 0 ) {
+            System.out.println(" Even Number ");
+        }
+        else {
+            System.out.println( "Odd Number ");
+
+        }
+        System.out.println( "Buy" );
+    }
+
 }
