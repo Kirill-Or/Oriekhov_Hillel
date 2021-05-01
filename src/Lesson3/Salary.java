9ebb48945b500a3d65b26ddf36a1edefd769bf56
Kirill <kr.oriekhov@gmail.com>
2021-05-02 00:07:45 +0300

    First commit

---
+++ src/Lesson3/Salary.java
@@ -0,0 +1,13 @@
+package Lesson3;
+
+public class Salary {
+    public static void main(String[] args) {
+        int salary= 1000;
+        int sales = 15;
+            if (sales > 10) {
+                salary += 250;
+            }
+        System.out.println( "Your salary is " + salary );
+
+    }
+}
