846b44965ff344c0b7efe2ea4e3df6d80579e5a7
Kirill <kr.oriekhov@gmail.com>
2021-05-03 18:59:16 +0300

    First commit

--- src/Lesson3/definition_of_numbers.java
+++ src/Lesson3/definition_of_numbers.java
@@ -6,25 +6,25 @@ public class definition_of_numbers {
     public static void main(String[] args) {
 
 
-        int a = 5;
+        int a = 9;
         int b = 10;
         int c = 9;
-        if (a > b & a > c)
-            if (b > c) {
+        if (a >= b & a >= c)
+            if (b >= c) {
                 System.out.println(a + " Max " + c + " Min " + b + " average"); }
-            else if (c > b) {
+            else if (c >= b) {
                 System.out.println(a + " Max " + b + " Min " + c + " average"); }
             else {}
-        else if (b > a & b > c)
-            if (a > c) {
+        else if (b >= a & b >= c)
+            if (a >= c) {
                     System.out.println(b + " Max " + c + " Min " + a + " average"); }
-            else if (c > a) {
+            else if (c >= a) {
                     System.out.println(b + " Max " + a + " Min " + c + " average"); }
             else {}
-        else if (c > a & c > b)
-            if (a > b)
+        else if (c >= a & c >= b)
+            if (a >= b)
                         System.out.println(c + " Max " + b + " Min " + a + " average");
-            else if (b > a) {
+            else if (b >= a) {
                         System.out.println(c + " Max " + a + " Min " + b + " average");
                     }
             else {}
