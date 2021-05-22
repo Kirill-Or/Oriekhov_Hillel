2e5a06657b8e87b21e53e21b0bae1373f3f202fa
Kirill <kr.oriekhov@gmail.com>
2021-05-22 19:48:08 +0300

    First commit

--- src/Lesson6/salaryAgent/SalaryAgentSales.java
+++ src/Lesson6/salaryAgent/SalaryAgentSales.java
@@ -2,11 +2,12 @@ package Lesson6.salaryAgent;
 
 public class SalaryAgentSales {
 
-     int hour ;
-     double ratePerHour ;
-     int experience ;
-     int numberOfSales ;
-     double salesAmount ;
+    int hour;
+    double ratePerHour;
+    int experience;
+    int numberOfSales;
+    double salesAmount;
+    double salary;
 
 
     public SalaryAgentSales(int hour, double ratePerHour, int experience, int numberOfSales, double salesAmount) {
@@ -15,21 +16,63 @@ public class SalaryAgentSales {
         this.experience = experience;
         this.numberOfSales = numberOfSales;
         this.salesAmount = salesAmount;
-
-
-
+        setSalary(overtimeBonus() + (ratePerHour * 160 * experienceCoefficient()) +
+                quantityBonus() + bonusForTheAmount());
 
     }
 
     public SalaryAgentSales(int hour, double ratePerHour) {
         this.hour = hour;
         this.ratePerHour = ratePerHour;
-
-
-
-
+        setSalary(overtimeBonus() + (ratePerHour * hour) );
     }
 
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public double getRatePerHour() {
+        return ratePerHour;
+    }
+
+    public void setRatePerHour(double ratePerHour) {
+        this.ratePerHour = ratePerHour;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getNumberOfSales() {
+        return numberOfSales;
+    }
+
+    public void setNumberOfSales(int numberOfSales) {
+        this.numberOfSales = numberOfSales;
+    }
+
+    public double getSalesAmount() {
+        return salesAmount;
+    }
+
+    public void setSalesAmount(double salesAmount) {
+        this.salesAmount = salesAmount;
+    }
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
     public double overtimeBonus() {
         if (hour > 160)
@@ -38,21 +81,20 @@ public class SalaryAgentSales {
             return 0;
         }
     }
-    public double experienceCoefficient () {
-        if (experience < 2){
+
+    public double experienceCoefficient() {
+        if (experience < 2) {
             return 1.0;
-        }
-        else if (experience >=2 & experience < 4) {
+        } else if (experience >= 2 & experience < 4) {
             return 1.2;
-        }
-        else if (experience >=4 & experience <6){
+        } else if (experience >= 4 & experience < 6) {
             return 1.3;
-        }
-        else
+        } else
             return 1.4;
 
-}
-    public double  quantityBonus () {
+    }
+
+    public double quantityBonus() {
         if (numberOfSales > 20) {
             return 250;
         } else if (numberOfSales < 10) {
@@ -61,18 +103,12 @@ public class SalaryAgentSales {
             return 0;
 
     }
-    public double bonusForTheAmount ( ){
+
+    public double bonusForTheAmount() {
         if (salesAmount > 15000) {
             return 1000;
-        }
-        else
+        } else
             return 0;
     }
-    public double getSalary( ){
-        return  ( overtimeBonus() + (   this.ratePerHour *  160 * experienceCoefficient()) +
-                quantityBonus()+ bonusForTheAmount());
-    }
-    public double getSalary(){
-        return (overtimeBonus() + (ratePerHour + hour));
-    }
+
 }
--- src/Lesson6/salaryAgent/SalaryCalculation.java
+++ src/Lesson6/salaryAgent/SalaryCalculation.java
@@ -2,8 +2,10 @@ package Lesson6.salaryAgent;
 
 public class SalaryCalculation {
     public static void main(String[] args) {
-        SalaryAgentSales salaryAgentSales = new SalaryAgentSales(5, 7 );
-        System.out.println( "your salary is :" + salaryAgentSales.getSalary( ) );
-        salaryAgentSales.
+        SalaryAgentSales salaryAgentSalesProbation = new SalaryAgentSales(160, 2 );
+        SalaryAgentSales salaryAgentSales= new SalaryAgentSales(160 ,2, 2, 15,15000  );
+        System.out.println("Salary on Probation :" + salaryAgentSalesProbation.getSalary());
+        System.out.println("Salary on Probation :" + salaryAgentSales.getSalary() );
+
     }
 }
