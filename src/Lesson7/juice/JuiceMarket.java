25bad339af2f6350204a51d5a7ec29b50720beca
Kirill <kr.oriekhov@gmail.com>
2021-05-29 23:00:16 +0300

    First commit

--- src/Lesson7/juice/JuiceMarket.java
+++ src/Lesson7/juice/JuiceMarket.java
@@ -3,22 +3,22 @@ package Lesson7.juice;
 public class JuiceMarket {
     public static void main(String[] args) {
         Apple apple = new Apple();
-        System.out.println(apple.getCalories());
+        System.out.println("Apple has " + apple.getCalories() + " calories ");
         apple.removeSeeds();
         apple.makeJuice();
 
         Banana banana = new Banana();
-        System.out.println(banana.getCalories());
+        System.out.println("Banana has " + banana.getCalories()+ " calories" );
         banana.peelBanana();
         banana.makeJuice();
 
         Orange orange = new Orange();
-        System.out.println(orange.getCalories());
+        System.out.println("Orange has " + orange.getCalories() +  " calories");
         orange.peelOrange();
         orange.makeJuice();
 
         Lemon lemon = new Lemon();
-        System.out.println(lemon.getCalories());
+        System.out.println("Lemon has " + lemon.getCalories() + " calories");
         lemon.peelLemon();
         lemon.makeJuice();
     }
