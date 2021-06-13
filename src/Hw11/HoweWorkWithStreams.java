049a208cba6ee94850c6c89c1410b869a0e6b3fe
Kirill <kr.oriekhov@gmail.com>
2021-06-13 08:36:44 +0300

    Second commit

--- src/Hw11/HoweWorkWithStreams.java
+++ src/Hw11/HoweWorkWithStreams.java
@@ -2,6 +2,7 @@ package Hw11;
 
 
 import java.util.Comparator;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -38,12 +39,17 @@ public class HoweWorkWithStreams {
         System.out.println(sortedByPrice);
 
 
-        Map <Integer , Computer>  map = computerList.stream().collect(Collectors.toMap(k-> k.getId() , v -> v ));
+        Map <Integer , Computer>  map = computerList.stream().collect(Collectors.toMap(Computer::getId, v -> v ));
 
 
+        boolean isDOCNotPresent = computerList.stream()
+                .noneMatch(computer -> computer.getOS() == "DOC");
+        System.out.println("Is there no DOC OS : " + isDOCNotPresent);
+
+        boolean isMacOSPresent = computerList.stream()
+                .anyMatch(computer -> computer.getOS() == "MacOS");
+        System.out.println("Is there any computers with MacOs : " + isMacOSPresent);
 
 
     }
-
-
 }
